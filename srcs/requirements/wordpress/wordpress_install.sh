6b6315d4d407235f7ddc16eb72eeb6c1631a716e
Daniel Lerma casin <dlerma-c@c3r4s5.42madrid.com>
2024-05-27 18:46:42 +0200

    FUNCIONA

---
+++ srcs/requirements/wordpress/wordpress_install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd /var/www/html
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+./wp-cli.phar core download --allow-root
+./wp-cli.phar config create --dbname=wordpress_inception --dbuser=dan_db --dbpass=dan123 --dbhost=mariadb_inception --allow-root
+./wp-cli.phar core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
+
+php-fpm7.4 -F
