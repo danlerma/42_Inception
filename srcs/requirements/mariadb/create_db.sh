85d53b82df695093a0a51dfbabeee7e554c1339b
Daniel Lerma casin <dlerma-c@c1r15s3.42madrid.com>
2024-06-03 19:15:41 +0200

    Intento de certificado

--- srcs/requirements/mariadb/create_db.sh
+++ srcs/requirements/mariadb/create_db.sh
@@ -2,3 +2,4 @@
 
 mysql_install_db
 mysqld
+mysql
--- srcs/requirements/wordpress/wordpress_install.sh
+++ srcs/requirements/wordpress/wordpress_install.sh
@@ -5,5 +5,5 @@ chmod +x wp-cli.phar
 ./wp-cli.phar core download --allow-root
 ./wp-cli.phar config create --dbname=wordpress_inception --dbuser=dan_db --dbpass=dan123 --dbhost=mariadb_inception --allow-root
 ./wp-cli.phar core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
-
+./wp-cli.phar theme install rainfall --activate --allow-root
 php-fpm7.4 -F
